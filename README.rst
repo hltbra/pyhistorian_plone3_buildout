221b089af76683605841b10f70d2b66e0045ccd5
Hugo Lopes Tavares <hltbra@gmail.com>
2010-08-27 17:05:22 -0300

    added a brief description of how to use it and commented out the lines related to doctest

---
+++ README.rst
@@ -0,0 +1,22 @@
+=======================
+ Pyhistorian + Plone 3
+=======================
+
+Yes, we are trying to run pyhistorian with Plone 3!
+
+
+Using this buildout
+===================
+
+$ python bootstrap.py
+$ bin/buildout
+
+
+Running myproduct tests::
+
+    $ bin/instance test -s myproduct
+
+
+
+PS.: I didn't get PIL installation working properly with buildout.cfg here,
+and installed both PIL and pyhistorian by hand.
--- src/myproduct/myproduct/myproduct/tests/test_doctest.py
+++ src/myproduct/myproduct/myproduct/tests/test_doctest.py
@@ -12,12 +12,12 @@ from myproduct.myproduct.tests import base
 def test_suite():
     return unittest.TestSuite([
 
-        # Demonstrate the main content types
-        ztc.ZopeDocFileSuite(
-            'README.txt', package='myproduct.myproduct',
-            test_class=base.FunctionalTestCase,
-            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE |
-                doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS),
+#        # Demonstrate the main content types
+#        ztc.ZopeDocFileSuite(
+#            'README.txt', package='myproduct.myproduct',
+#            test_class=base.FunctionalTestCase,
+#            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE |
+#                doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS),
 
         ])
 
